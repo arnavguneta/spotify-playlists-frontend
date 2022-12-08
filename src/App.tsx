7e689d02b46a0bb12da32bc7c33f8329d0e0ac2b
Arnav Guneta <arnavguneta@gmail.com>
2022-12-07 21:02:02 -0500

    cors error fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import './App.css';
 
 function App() {
   const onLogin: MouseEventHandler<HTMLButtonElement> = async () => {
-    window.location.replace('http://localhost:3000/api/v1/spotify/auth/login');
+    window.location.replace(`${process.env.REACT_APP_BACKEND_API}/auth/login`);
   };
-
   const onFetchProfile: MouseEventHandler<HTMLButtonElement> = async () => {
     const response = await fetch(
-      'http://localhost:3000/api/v1/spotify/user');
+      `${process.env.REACT_APP_BACKEND_API}/user`, { credentials: 'include' }
+    );
     return await response.json();
   };
 
   const onLogout: MouseEventHandler<HTMLButtonElement> = async () => {
     const response = await fetch(
-      'http://localhost:3000/api/v1/spotify/auth/logout');
+      `${process.env.REACT_APP_BACKEND_API}/auth/logout`,
+      { credentials: 'include' }
+    );
     return await response.json();
   };
 
   return (
     <div className="App">
+      <img src={`${process.env.PUBLIC_URL}/images/logo/spotify.svg`} />
       <h1>Test</h1>
       <button onClick={onLogin}>Log in</button>
       <button onClick={onFetchProfile}>Fetch profile</button>
